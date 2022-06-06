43d8417d2852068dd441de1baf4c9c50be9bb522
Pratik Sharma <sharma.pratik2016@gmail.com>
2022-06-06 14:14:21 +0530

    created calculator files

---
+++ calculator/target/debug/calculator.d
@@ -0,0 +1,1 @@
+/Users/coolhead/Documents/learning/rust-by-march/calculator/target/debug/calculator: /Users/coolhead/Documents/learning/rust-by-march/calculator/src/main.rs
---
+++ calculator/target/rls/debug/deps/calculator-9ed31aa3bf06df86.d
@@ -0,0 +1,5 @@
+/Users/coolhead/Documents/learning/rust-by-march/calculator/target/rls/debug/deps/calculator-9ed31aa3bf06df86.rmeta: src/main.rs
+
+/Users/coolhead/Documents/learning/rust-by-march/calculator/target/rls/debug/deps/calculator-9ed31aa3bf06df86.d: src/main.rs
+
+src/main.rs:

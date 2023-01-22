f4b4eeedf883701cf07eb241be9b1383ee6b0c4c
Pratik Sharma <sharma.pratik2016@gmail.com>
2023-01-22 14:55:20 +0530

    parsing http from buffer and then to string

--- http-server/src/http/request.rs
+++ http-server/src/http/request.rs
@@ -14,17 +14,31 @@ impl TryFrom<&[u8]> for Request {
     type Error = ParseError;
 
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
-        match str::from_utf8(buf) {
-            Ok(request) => {}
-            Err(_) => return Err(ParseError::InvalidEncoding),
-        };
+      
         let request = str::from_utf8(buf)?;
+
+        let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+
+        if(protocol != "HTTP/1.1") {
+            return  Err(ParseError::InvalidProtocol);
+        }
+
+
+
+        match get_next_word(request) {
+            Some((method, request)) => {}, 
+            None => return Err(ParseError::InvalidRequest)
+            
+        }
+
         unimplemented!()
     }
 }
 fn get_next_word(request:&str) -> Option<(&str, &str)> {
     for (i, c) in request.chars().enumerate() {
-        if c == ' ' {
+        if c == ' ' || c == '\r' {
             return Some((&request[..i], &request[i+1..]));
         }
     }

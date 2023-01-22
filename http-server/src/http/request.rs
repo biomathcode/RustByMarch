cec3db839a3db693ebe74c9c3d26571e1db7ed80
Pratik Sharma <sharma.pratik2016@gmail.com>
2023-01-22 14:39:19 +0530

    Error handling and convertions

--- http-server/src/http/request.rs
+++ http-server/src/http/request.rs
@@ -1,6 +1,70 @@
 use super::method::Method;
+use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+use std::str;
+use std::str::Utf8Error;
 pub struct Request {
     path: String,
     query_string: Option<String>,
     method: Method,
 }
+
+impl TryFrom<&[u8]> for Request {
+    type Error = ParseError;
+
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        match str::from_utf8(buf) {
+            Ok(request) => {}
+            Err(_) => return Err(ParseError::InvalidEncoding),
+        };
+        let request = str::from_utf8(buf)?;
+        unimplemented!()
+    }
+}
+fn get_next_word(request:&str) -> Option<(&str, &str)> {
+    for (i, c) in request.chars().enumerate() {
+        if c == ' ' {
+            return Some((&request[..i], &request[i+1..]));
+        }
+    }
+    None
+}
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "Invalid Request",
+            Self::InvalidEncoding => "Invalid Encoding",
+            Self::InvalidProtocol => "Invalid Protocol",
+            Self::InvalidMethod => "Invalid Method",
+        }
+    }
+}
+
+impl From<Utf8Error> for ParseError {
+    fn from(_:Utf8Error) -> Self {
+        Self::InvalidEncoding
+    };
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Error for ParseError {}
--- http-server/src/server.rs
+++ http-server/src/server.rs
@@ -1,3 +1,6 @@
+use crate::http::{request, Request};
+use std::convert::TryFrom;
+use std::io::Read;
 use std::net::TcpListener;
 
 pub struct Server {
@@ -11,9 +14,30 @@ impl Server {
 
     pub fn run(self) {
         println!("Listening on port: {}", self.addr);
-        // unwrap return the result value and if it is an error 
+        // unwrap return the result value and if it is an error
         let listener = TcpListener::bind(&self.addr).unwrap();
 
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
 
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {}
+                                Err(e) => println!("Failed to read from connection: {}", e),
+                            }
+                            let res: &Result<Request, _> = &buffer[..].try_into();
+                        }
+                        Err(e) => println!("Failed to read from connection: {}", e),
+                    }
+                }
+                Err(e) => {
+                    println!("err: {}", e)
+                }
+            }
+        }
     }
 }
